b03aa2c447fd7e11dfefd0b49ba063de261f9143
spidey-rishav <r4rishavmodi@gmail.com>
2026-01-08 15:43:17 +0530

    running server

--- node_express/main.js
+++ node_express/main.js
@@ -3,11 +3,24 @@
 // const result = add(10, 20)
 // console.log("sum of 2 numbers: " + result)
 
-http = require('http') //core module
-server = http.createServer((req, res) => {
-    res.end("hi this is server!")
-})
+//..........................................
+// http = require('http') //core module
+// server = http.createServer((req, res) => {
+//     res.end("hi this is server!")
+// })
 
-server.listen(3000,
-    console.log("server is running...")
-)
+// server.listen(3000,
+//     console.log("server is running...")
+// )
+
+//.....................................
+file = require('fs')
+
+file.readFile('abc.txt', 'utf-8', (err, data) => {
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log(`reading data: ${data}`);
+    }
+})
